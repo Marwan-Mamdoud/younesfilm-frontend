27f53fed585ccb0ae67ec31e2ebc35a5b9edf5c4
Marwan <marwanmamdouh169@gmail.com>
2024-12-19 00:01:30 +0200

    edit

--- app/add/page.jsx
+++ app/add/page.jsx
@@ -379,7 +379,7 @@ const Page = () => {
                 <div className="grid grid-cols-2 crew items-center justify-center gap-5">
                   <input
                     type="text"
-                    defaultValue="Client"
+                    defaultValue="Post Production"
                     placeholder="Enter crew of project"
                     className="w-full crewsen name rounded-md bg-white text-black px-5 h-10 outline-none"
                   />
